b4cb5f6c01beca7997dbfb50467c768c235a7407
Xyntexx <21282105+Xyntexx@users.noreply.github.com>
2025-10-05 19:43:48 +0300

    Fix keyboard button overlapping after .NET 8 migration
    Changed AutoScaleMode from Font to Dpi in all keypad controls to fix
    button layout issues caused by .NET 8 font scaling behavior changes.
    Updated AutoScaleDimensions to (96F, 96F) for proper DPI handling.
    
    Fixes overlapping buttons especially in first row.

--- SourceCode/Keypad/Keyboard.Designer.cs
+++ SourceCode/Keypad/Keyboard.Designer.cs
@@ -1066,9 +1066,9 @@
             this.d12.Click += new System.EventHandler(this.BtnClick);
             // 
             // Keyboard
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.Controls.Add(this.d12);
             this.Controls.Add(this.d11);
